d6d711050c1e8fec9f9f5c52f3a0c4cb663f5765
Umesh Kumawat <141519505+umeshkumawat7@users.noreply.github.com>
2025-04-29 06:28:18 +0000

    variable complete

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 458669
+let accountEmail = "umesh404@gmail.com"
+var accountPassword = "01125532553"
+accountCity = "Indore"
+let branchName;
+
+//accountId = 919191
+
+console.log(accountId);
+accountEmail = "error404@gmail.com"
+accountPassword = "+9101125532553"
+
+/*
+ prefer not to use var
+ because of issue in block scope and functional scope
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity,branchName])
