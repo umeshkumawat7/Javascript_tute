5aa5d2851f644274645057d45a419c08e31187d3
Umesh Kumawat <141519505+umeshkumawat7@users.noreply.github.com>
2025-05-02 10:24:40 +0000

    dataType complete

---
+++ 01_Basic/02_variable.js
@@ -0,0 +1,25 @@
+"use strict";
+//treat all code as newer js version
+
+//alert(3+3)
+//we are using node not browser
+
+let name = "umesh"
+let age = 26
+let isLoggedin = false
+
+//number type => 2 to power 53
+// Bigint
+// string => ""
+// Boolean => true/false
+// null=> standlon value
+//undefined;
+//symbol => unique
+
+//object type
+
+console.log(typeof  "umesh")
+//typeof => for checking type of data
+
+console.log(typeof  null) //asked in interview type of null
+console.log(typeof  undefined)
