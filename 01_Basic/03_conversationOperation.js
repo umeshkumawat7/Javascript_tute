9e9139e2ac70be35e515ba8d887323bb1b028d72
Umesh Kumawat <141519505+umeshkumawat7@users.noreply.github.com>
2025-05-03 10:43:23 +0000

    typeCasting

---
+++ 01_Basic/03_conversationOperation.js
@@ -0,0 +1,30 @@
+//Conversation In Number
+let score = undefined//null//true//"abc"//"33abc"//"33" //33
+
+// console.log(score)
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// "33" => 33
+// ""   => 0
+//"33abc" => NaN
+//"abc" => NaN
+// true => 1; false => 0
+// null => 0
+//undefined => NaN
+
+let isLoggedIn = "abc"//1//true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+//1 => true; 0 =>false
+//""=> false; "umesh"=> true
+
+let someValue = true//33
+let valueInString = String(someValue);
+
+console.log(valueInString);
+console.log(typeof valueInString);
