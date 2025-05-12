540cafaded1227cc3b9ff0f03b238fedc4339567
Umesh Kumawat <141519505+umeshkumawat7@users.noreply.github.com>
2025-05-12 07:32:50 +0000

    comparision

---
+++ 01_Basic/04_comparesion.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("2"<1);
+// console.log(2<"1");
+// console.log(2>"1");  //comparision operater first conver str into Num
+
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(null<0);
+console.log(null>0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined != 0);
+
+// avoid both type of comparision null & undefined
