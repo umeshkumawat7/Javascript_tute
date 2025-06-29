3122b0f1e400fa90e1e11bc1841abdd391c0be16
Umesh Kumawat <141519505+umeshkumawat7@users.noreply.github.com>
2025-06-29 20:45:47 +0000

    object leve 2

---
+++ 02_Basic/obj2.js
@@ -0,0 +1,37 @@
+const tinderUser = new Object() //singalton
+tinderUser.name = 'Umesh'
+tinderUser.age = 22
+tinderUser.email = 'some@gmail.com'
+tinderUser.isLoggedIn = true
+//console.log(tinderUser);
+
+const newUser ={
+    userDetails:{
+        userFullName:{
+            fname:'Umesh',
+            lname:'Kumawat'
+        }
+    }
+}
+//console.log(newUser.userDetails.userFullName.fname);
+
+const obj1 = {a:1,b:2}
+const obj2 = {c:3,d:4}
+//for merge two object
+//const obj3 = Object.assign({},obj1, obj2) //this is {} for strict target we want this , also assign is old way now we use this
+const obj3 = {...obj1,...obj2} // we use spread operator
+//console.log(obj3);
+
+console.log(tinderUser);
+console.log(Object.entries(tinderUser)); // return array of key value pair inside nested array form
+
+console.log(Object.keys(tinderUser)); // return array of keys in objject
+console.log(Object.values(tinderUser));// return array of values in object
+console.log(Object.hasOwn(tinderUser,'name'));//in (objectName,keyName) we pass and check it existing or not and return bool value
+console.log(tinderUser.hasOwnProperty('name'));//both method work same, it check if given key is existing or not and given bool output
+
+const user = [{id:1,email:'some1@mail.com'},
+    {id:2,email:'some2@mail.com'},
+    {id:3,email:'some3@mail.com'}
+]       //this is another methode we recived multiple object in array
+console.log(user[1].email);
